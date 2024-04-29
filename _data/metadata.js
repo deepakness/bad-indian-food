c412711e4979b14e6a5e7e464676aea493f53fe0
deepakness <deepakkr342@gmail.com>
2024-04-29 11:49:22 +0530

    updated url

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,7 +1,7 @@
 module.exports = {
     "website_name": "BadFoods",
     "website_description": "BadFoods has a list of Indian products banned in EU.",
-    "website_url": "https://badfoods.com",
+    "website_url": "https://badfoods.netlify.app",
     "website_language": "en",
     "website_author": {
         "name": "DeepakNess",
